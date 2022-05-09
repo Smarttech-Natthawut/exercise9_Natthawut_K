c63f939b3843c66197e738b9255a92816349d097
Smarttech-Natthawut <nattawut.smarttech@gmail.com>
2022-05-09 16:42:00 +0700

    Create exercise9_Natthawut_K.py

---
+++ exercise9_Natthawut_K.py
@@ -0,0 +1,6 @@
+usernameInput = input("Username :")
+passwordInput = input("Password :")
+while usernameInput != "admin"  or passwordInput !="1234":
+    usernameInput = input("Plears in put Username :")
+    passwordInput = input("Please in put Password :")
+print ("Done !!!")
